bf985d2d3a9f5f47e618ab5a0c59e60d77473b02
morganhel <138800018+morganhel@users.noreply.github.com>
2024-01-03 13:23:04 +0100

    Mise en place bannière page a propos

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,3 +1,5 @@
+
+
 function Card ({image,title}){
 
     return (
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,5 +1,5 @@
-import Logo from '../../assets/images/LOGO.svg'
-import { Link } from 'react-router-dom'
+import Logo from '../../assets/images/LOGO.svg';
+import { Link } from 'react-router-dom';
 
  
 function Header() {
